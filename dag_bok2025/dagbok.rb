e1c99f15560b035af1d08ddea58fc4a12a45302f
Elias-johnsson <elias.johnsson5@outlook.com>
2025-04-10 11:43:51 +0200

    lektion 2

--- dag_bok2025/dagbok.rb
+++ dag_bok2025/dagbok.rb
@@ -1,3 +1,4 @@
+
 def password_try()
   correct_password = File.read("password.txt")
   value = false
@@ -15,18 +16,88 @@ def password_try()
     end
 end
 
-def dagbok_start()
-  value = false
-  while value != true
-    #password_try()
-    puts "Do you want to Read or Write?"
-    input = gets.chomp
-    #Validering 
-    if input.downcase == "read"
-      #readfunc
-    elsif input.downcase == "write"
-      #writefunc
-    else
-      puts "Please enter a valid input"
+
+def readfunc()
+  puts "what date do you want to read(YY_MM_DD)?"
+  date = gets.chomp() + ".txt"
+  Dir.chdir("entries")
+  open_date = File.read(date)
+  puts " "
+  puts "_______________________________________________________________________"
+  puts " "
+  puts open_date
+  puts " "
+  puts "_______________________________________________________________________"
+  puts " "
+  valueread = false 
+  while valueread != true 
+    puts "do you want to log out or read another date?"
+    input = gets.chomp.strip()
+    if input == "logout"
+      valueread = true 
+    elsif input == "read"
+      valueread = true
+    else 
+      puts "Input must be an alternativ"
     end
   end
+  Dir.chdir("../")
+  if input == "read"
+    readfunc()
+  end
+end
+
+
+def writefunc()
+  puts "Input todays date(YY_MM_DD): "
+  name = gets.chomp + ".txt"
+  puts " "
+  puts "_______________________________________________________________________"
+  puts " "
+  Dir.chdir("entries")
+  file = File.open(name,"w")
+  puts "File is in write mode, click 'enter' to submit line, type '/end' to stop writing"
+  writing = true
+  puts "Type the entry: "
+  puts " "
+  while writing
+    text_line = gets.chomp
+    if text_line == "/end"
+      writing = false
+      file.close
+      puts " "
+      puts "_______________________________________________________________________"
+      puts " "
+      puts "Writing mode has ended"
+    else
+      file.puts(text_line)
+    end
+  end
+Dir.chdir("../")
+end
+
+def main()
+  value = true
+  condition = true
+  while condition
+    #password_try()
+    while value != false
+      puts "Do you want to Read,Write or logout?"
+      input = gets.chomp
+      #Validering 
+      if input.downcase == "read"
+        readfunc()
+      elsif input.downcase == "write"
+        writefunc()
+      elsif input.downcase == "logout"
+        value = false
+      else
+        puts "Input must be an alternativ"
+      end
+    end
+  end
+end
+
+main()
+
+
